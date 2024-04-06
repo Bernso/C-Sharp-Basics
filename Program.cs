6da5637e2cd378fe11714119040ce613f35ee632
benwsheridan <ben.w.sheridan@gmail.com>
2024-04-06 22:26:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+/*
+int     - stores integers (whole numbers), without decimals, such as 123 or -123
+double  - stores floating point numbers, with decimals, such as 19.99 or -19.99
+char    - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+string  - stores text, such as "Hello World". String values are surrounded by double quotes
+bool    - stores values with two states: true or false
+int	      4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+long	  8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+float	  4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+double	  8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+bool	  1 bit	Stores true or false values
+char	  2 bytes	Stores a single character/letter, surrounded by single quotes
+string	  2 bytes per character	Stores a sequence of characters, surrounded by double quotes
+*/
+
+
+namespace Program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+            // The least complex addition in C# to date
+            Console.WriteLine("A simple addition program: ");
+            Console.Write("Enter a number: ");
+            string number1 = Console.ReadLine();
+            Console.Write("Enter another number: ");
+            string number2 = Console.ReadLine();
+            int total = Convert.ToInt32(number1) + Convert.ToInt32(number2);
+            Console.WriteLine(number1 + " + " + number2 + " = " + total);
+            */
+
+
+            /*
+            // Example of all the data types
+            int myNum = 5;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+            */
+
+            /*
+            // Gets userinput and prints it out
+            string thing_to_say = Console.ReadLine();
+            Console.WriteLine(thing_to_say);
+            */
+
+
+            /*
+            Console.WriteLine("Hello world!");  // Writes hello world on one line and on a new line it writes omg twice
+            Console.Write("omg ");
+            Console.Write("omg");
+            */
+
+
+        }
+    }
+}
